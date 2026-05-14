103d826a2e629e039b97703e12509c16086a7119
praveen-j25 <praveen-j25@users.noreply.github.com>
2026-05-14 16:09:25 +0530

    Fix database connection by using Render dynamic service variables

--- TaskManagerMVC/Program.cs
+++ TaskManagerMVC/Program.cs
@@ -22,6 +22,18 @@ builder.Services.AddAntiforgery(options =>
 
 // Database - ADO.NET
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
+
+// Support for Render environment variables
+var dbHost = Environment.GetEnvironmentVariable("DB_HOST");
+if (!string.IsNullOrWhiteSpace(dbHost))
+{
+    var dbPort = Environment.GetEnvironmentVariable("DB_PORT") ?? "3306";
+    var dbName = Environment.GetEnvironmentVariable("DB_NAME") ?? "task_manager_db";
+    var dbUser = Environment.GetEnvironmentVariable("DB_USER") ?? "root";
+    var dbPass = Environment.GetEnvironmentVariable("DB_PASSWORD") ?? "ChangeMe123!";
+    connectionString = $"Server={dbHost};Port={dbPort};Database={dbName};User={dbUser};Password={dbPass};SslMode=None;AllowPublicKeyRetrieval=true;";
+}
+
 builder.Services.AddSingleton(new DbConnectionFactory(connectionString!));
 builder.Services.AddTransient<DatabaseSeeder>();
 
